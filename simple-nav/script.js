6132df11e6c36a0e4f70ef585ac1d8dd73de7274
Kashish Thakur <abhimanhas2132@gmail.com>
2026-03-09 09:52:06 +0530

    Update script.js

--- simple-nav/script.js
+++ simple-nav/script.js
@@ -1,4 +1,13 @@
 const navLinks = document.getElementById("navLinks");
+const menuBtn = document.getElementById("menuBtn");
+
 function toggleMenu(){
   navLinks.classList.toggle("active");
+
+  // change icon
+  if (navLinks.classList.contains("active")) {
+    menuBtn.textContent = "✕"; // cross
+  } else {
+    menuBtn.textContent = "☰"; // menu icon
+  }
 }
